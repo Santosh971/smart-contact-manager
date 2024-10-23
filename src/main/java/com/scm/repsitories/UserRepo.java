04ed37c5b24fd8a9d30716b387b6f8c6475e3a52
Santosh971 <santoshshimpankar812@gmail.com>
2024-10-23 11:10:06 +0530

    Initial commit

---
+++ src/main/java/com/scm/repsitories/UserRepo.java
@@ -0,0 +1,21 @@
+package com.scm.repsitories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scm.entities.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, String> {
+
+    // Our custom methods
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+    Optional<User> findByEmailToken(String id);
+
+}
